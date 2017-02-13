d99f31dea25f0b283dca41f5e6106b175d522e62
Jennifer Winer <jeniwiner@gmail.com>
2017-02-12 18:02:30 -0700

    add reset button and make responsive

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
   let grid = document.createElement('div');
   grid.classList.add('grid');
 
- //create 2x2 grid
+  //create 2x2 grid
   for (let i = 0; i < 20; i++) {
     let row = document.createElement('div');
     //adding class to each row, 'row'
@@ -21,6 +21,7 @@
       var column = document.createElement('div');
       //adding class to each div within each row, 'column'
       column.setAttribute('id', 'column');
+      column.setAttribute('name', 'square');
       // column.classList.add('a');
       row.appendChild(column);
     }
@@ -28,21 +29,35 @@
 
   pixels.appendChild(grid);
 
- let body = document.getElementsByTagName('body')[0];
+  let body = document.getElementsByTagName('body')[0];
 
- let color = '';
+  let squares = document.getElementsByName('square');
 
- body.addEventListener('click', function() {
-  if (event.target.classList[0] === 'paint') {
-    color = event.target.classList[1];
-    colorIndicator.className = color;
-  } if (event.target.hasAttribute('id', 'column')) {
+  let color = '';
+  // let column = document.getElementById('column');
+
+  body.addEventListener('click', function() {
+    if (event.target.classList[0] === 'paint') {
+      color = event.target.classList[1];
+      colorIndicator.className = color;
+    }
+    if (event.target.hasAttribute('id', 'column')) {
       event.target.className = color;
-  }
- });
+    }   if (event.target === btn) {
+        for (square of squares) {
+          square.className = 'gray1';
+        }
+      }
+  });
 
   let colorIndicator = document.getElementById('colorIndicator');
-  console.log(colorIndicator);
+
+  //creating reset btn
+  let footer = document.getElementsByTagName('footer')[0];
+  let btn = document.createElement('button');
+  let quote = document.getElementById('quote');
+  btn.innerText = 'Clear the Canvas';
+  footer.appendChild(btn);
 
 
 })();
