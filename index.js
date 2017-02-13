90ebd0e7a0a67f2b12b2f343d7ccfbc790897c12
Jennifer Winer <jeniwiner@gmail.com>
2017-02-13 13:39:15 -0700

    pixel art for submission

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 (function() {
 
-  // console.log(document);
-  //getting pixels and palette divs
+
+  //get pixels and palette divs
   let pixels = document.getElementsByClassName('pixels')[0];
   let palette = document.getElementsByClassName('palette')[0];
 
-  // console.log(pixels[0]);
-
+  //create div to hold palette, add class 'grid'
   let grid = document.createElement('div');
   grid.classList.add('grid');
 
-  //create 2x2 grid
+  //create 20x30 grid
   for (let i = 0; i < 20; i++) {
     let row = document.createElement('div');
     //adding class to each row, 'row'
@@ -20,44 +19,56 @@
     for (let j = 0; j < 30; j++) {
       var column = document.createElement('div');
       //adding class to each div within each row, 'column'
-      column.setAttribute('id', 'column');
-      column.setAttribute('name', 'square');
-      // column.classList.add('a');
+      column.className = 'column';
       row.appendChild(column);
     }
   }
-
+  //append grid to pixels (i.e. the document)
   pixels.appendChild(grid);
 
   let body = document.getElementsByTagName('body')[0];
 
-  let squares = document.getElementsByName('square');
-
+  //create color variable to hold color classes
   let color = '';
-  // let column = document.getElementById('column');
 
-  body.addEventListener('click', function() {
-    if (event.target.classList[0] === 'paint') {
-      color = event.target.classList[1];
-      colorIndicator.className = color;
-    }
-    if (event.target.hasAttribute('id', 'column')) {
-      event.target.className = color;
-    }   if (event.target === btn) {
-        for (square of squares) {
-          square.className = 'gray1';
-        }
-      }
-  });
+  //create squares variable which is all of the squares in the grid
+  let squares = document.getElementsByClassName('column');
 
+  //get colorIndicator from html
   let colorIndicator = document.getElementById('colorIndicator');
 
-  //creating reset btn
+  //create reset btn
   let footer = document.getElementsByTagName('footer')[0];
   let btn = document.createElement('button');
   let quote = document.getElementById('quote');
   btn.innerText = 'Clear the Canvas';
   footer.appendChild(btn);
 
+
+  //event listeners for:
+  //paint, grid, reset
+
+  body.addEventListener('click', function() {
+
+    if (event.target.classList[0] === 'paint') {
+      color = event.target.classList[1];
+      colorIndicator.className = color;
+    }
+
+    if (event.target.classList[0] === 'column') {
+      if (event.target.classList[1] !== undefined) {
+        event.target.classList.remove(event.target.classList[1]);
+      }
+      event.target.classList.add(color);
+    }
+    if (event.target === btn) {
+      for (square of squares) {
+        if (square.classList[1] !== undefined) {
+          square.classList.remove(square.classList[1]);
+        }
+        square.classList.add('gray1');
+      }
+    }
+  });
 
 })();
